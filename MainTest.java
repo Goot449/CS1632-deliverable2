c75ae17501ea8be7e7ee9a0eff582b0524c07829
Jake <jake.guttenplan@gmail.com>
2016-06-12 16:27:18 -0400

    Add Main test

---
+++ MainTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jake on 6/12/2016.
+ */
+public class MainTest
+{
+	@org.junit.Before
+	public void setUp() throws Exception
+	{
+
+	}
+
+	@org.junit.After
+	public void tearDown() throws Exception
+	{
+
+	}
+
+	@org.junit.Test
+	public void main() throws Exception
+	{
+
+	}
+
+}
