890ed9769cd29e31f872f157ca8544997a4ce290
Jake <jake.guttenplan@gmail.com>
2016-06-12 16:24:15 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		// Make sure only a single argument was specified
+		if(args.length!=1)
+		{
+			System.out.println("Please specify an Integer as a seed value");
+			return;
+		}
+
+		// Get seed from command line
+		int seed;
+		try
+		{
+			seed=Integer.parseInt(args[0]);
+		}
+		catch(NumberFormatException nfe)
+		{
+			System.out.println("Single integer not specified");
+			return;
+		}
+		// set rng to be the random number based off the seed.
+		Random rng=new Random(seed);
+		System.out.println("seed" + rng);
+	}
+}
