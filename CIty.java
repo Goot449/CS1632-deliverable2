9f49c38c05f8f611c27b935c1a7626f5a9091187
Jake <jake.guttenplan@gmail.com>
2016-06-12 23:33:03 -0400

    implement roads into city

--- CIty.java
+++ CIty.java
@@ -6,14 +6,11 @@ import java.util.ArrayList;
 public class City
 {
 	private List<String> _locs = new ArrayList<String>();
-
-
-	/*
-	 * Add a destination to the city
-	 */
+	private List<Road> _roads = new ArrayList<Road>();
+	//Add location to city
 	public void addLocation(String loc)
 	{
-		// Add location if it doesn't already exist
+		// Add location only if it doesn't already exist
 		if (!_locs.contains(loc))
 		{
 			_locs.add(loc);
@@ -23,6 +20,37 @@ public class City
 	public int getNumLocations()
 	{
 		return _locs.size();
+	}
+	public void paveRoad(Road f)
+	{
+		// Add locations
+		addLocation(f.getSource());
+		addLocation(f.getDest());
+
+		//Add road once origin and destination exist
+		if (!_roads.contains(f))
+		{
+			_roads.add(f);
+		}
+	}
+	public int getNumRoads()
+	{
+		return _roads.size();
+	}
+
+	public List<Road> getOutgoingRoads(String src)
+	{
+		List<Road> outgoing = new ArrayList<Road>();
+
+		for (Road r : _roads)
+		{
+			if (r.getSource() == src)
+			{
+				outgoing.add(r);
+			}
+		}
+
+		return outgoing;
 	}
 
 }
