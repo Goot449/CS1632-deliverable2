3b11fc34997fae66a8f7b404a1767a76e6409ce5
Jake <jake.guttenplan@gmail.com>
2016-06-12 23:30:43 -0400

    implement roads into city

--- RoadTest.java
+++ RoadTest.java
@@ -8,21 +8,27 @@ import static org.junit.Assert.*;
 public class RoadTest
 {
 	@Test
-	public void getSource() throws Exception
+	public void getGetName() throws Exception
 	{
+		Road r = new Road("road", "source", "destination");
 
+		assertEquals(r.getName(), "road");
 	}
 
 	@Test
-	public void getName() throws Exception
+	public void testGetDest() throws Exception
 	{
+		Road r = new Road("road", "source", "destination");
 
+		assertEquals(r.getDest(), "destination");
 	}
 
 	@Test
-	public void getDest() throws Exception
+	public void getGetSource() throws Exception
 	{
+		Road r = new Road("road", "source", "destination");
 
+		assertEquals(r.getSource(), "source");
 	}
 
 }
